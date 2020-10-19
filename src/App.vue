abece6617fc39a15c765cd96078e07969b40cbd2
tran thanh quang <quang.tran290699@gmail.com>
2020-10-19 11:00:02 +0700

    fix: font-family for title

--- src/App.vue
+++ src/App.vue
@@ -42,9 +42,10 @@ export default {
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Commissioner&display=swap');
 html,
 body {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Commissioner', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   padding: 0;
