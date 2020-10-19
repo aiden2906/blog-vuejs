dd3b8b736cd57dcc5fd1174905c6fed709e52e0e
tran thanh quang <quang.tran290699@gmail.com>
2020-10-19 11:17:44 +0700

    fix: font-family markdown

--- src/components/view/Blog-Detail.vue
+++ src/components/view/Blog-Detail.vue
@@ -1,6 +1,6 @@
 <template>
   <div :style="{ width: '100%', padding: '60px' }" class="markdown-body">
-    <vue-markdown :source="article" :html="true" class="custom-markdown"></vue-markdown>
+    <vue-markdown :source="article" :html="true"></vue-markdown>
   </div>
 </template>
 <script>
@@ -35,8 +35,5 @@ export default {
   .markdown-body {
     padding: 15px;
   }
-}
-.custom-markdown {
-  font-family: 'Commissioner', sans-serif;
 }
 </style>
