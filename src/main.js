13bdb469cb74e58cdb03b38b6fdd4b6311f75d25
tran thanh quang <quang.tran290699@gmail.com>
2020-10-20 23:19:52 +0700

    feat: add vuex

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './routes/index.js';
 import ElementUI from 'element-ui';
+import { store } from './store/index.js';
 import 'element-ui/lib/theme-chalk/index.css';
-
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 
 new Vue({
   render: (h) => h(App),
   router,
+  store,
 }).$mount('#app');
